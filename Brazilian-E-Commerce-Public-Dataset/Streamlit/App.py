82306d7e86572094a754c2bdb41f9bb3b0f46227
Hylmi Razzan <142045581+HylmiRazzan@users.noreply.github.com>
2026-09-05 16:18:36 +0700

    Update App.py

--- Brazilian-E-Commerce-Public-Dataset/Streamlit/App.py
+++ Brazilian-E-Commerce-Public-Dataset/Streamlit/App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import os
 from deep_translator import GoogleTranslator
 
 st.set_page_config(page_title="Prediksi NPS", layout="centered")
